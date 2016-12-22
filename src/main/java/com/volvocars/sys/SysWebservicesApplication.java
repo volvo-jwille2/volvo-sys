01b0c95ae44c86914a9044539ab2670f42e236b2
volvo-jwille2 <jonas.wille@volvocars.com>
2016-12-22 09:41:33 +0100

    Update SysWebservicesApplication.java

--- src/main/java/com/volvocars/sys/SysWebservicesApplication.java
+++ src/main/java/com/volvocars/sys/SysWebservicesApplication.java
@@ -3,6 +3,8 @@ package com.volvocars.sys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +12,14 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
-public class SysWebservicesApplication {
+public class SysWebservicesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SysWebservicesApplication.class, args);
 	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SysWebservicesApplication.class);
+    }
 }
